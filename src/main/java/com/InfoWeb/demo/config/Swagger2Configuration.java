fe496cc9519af23aa0a21b8707e27a0bc2a3f4a8
b <gymnopediece@163.com>
2019-04-28 14:37:17 +0800

    browser

--- src/main/java/com/InfoWeb/demo/config/Swagger2Configuration.java
+++ src/main/java/com/InfoWeb/demo/config/Swagger2Configuration.java
@@ -12,11 +12,11 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-//@Configuration
-//@EnableSwagger2
+@Configuration
+@EnableSwagger2
 class Swagger2Configuration {
 
-//    @Bean
+    @Bean
     public Docket createRestApi(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
@@ -29,9 +29,9 @@ class Swagger2Configuration {
     private ApiInfo apiInfo(){
         return new ApiInfoBuilder()
                 .title("Spring Boot 中使用Swagger2构建RESTful APIs")
-                .description("学习Sping Boot例子， 我的Github: https://github.com/CycloneBoy")
+                .description("学习Sping Boot例子， 我的Github: https://github.com/bk")
                 .termsOfServiceUrl("https://github.com/CycloneBoy")
-                .contact(new Contact("CycloneBoy","https://github.com/CycloneBoy","xuanfeng1992@gmail.com"))
+                .contact(new Contact("bk","https://github.com/bk","bk@gmail.com"))
                 .version("1.0")
                 .build();
     }
